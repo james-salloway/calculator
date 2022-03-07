728fd00d55e2fbdba70277bb0096c049a42bb9a8
James Salloway <james@jmsalloway.co.uk>
2022-03-07 15:44:46 +0000

    Scaffold CalculatorController with expected methods, their required params, and their return values

---
+++ src/Controllers/CalculatorController.ts
@@ -0,0 +1,26 @@
+class CalculatorController {
+
+    static operate() {
+
+    }
+
+    static add(values: string): number {
+        
+    }
+
+    static subtract(values: string): number {
+
+    }
+
+    static multiply(values: string): number {
+
+    }
+
+    static divide(values: string): number {
+        
+    }
+
+}
+
+
+export default CalculatorController;
